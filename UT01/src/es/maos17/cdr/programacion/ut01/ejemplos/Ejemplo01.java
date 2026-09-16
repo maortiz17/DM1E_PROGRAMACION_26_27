70d36bd4ad52d92ed3ba8d2713af26db8c7d19a2
Miguel Angel Ortiz <maos17@educa.madrid.org>
2026-09-16 12:14:33 +0200

    Primer ejemplo

--- UT01/src/es/maos17/cdr/programacion/ut01/ejemplos/Ejemplo01.java
+++ UT01/src/es/maos17/cdr/programacion/ut01/ejemplos/Ejemplo01.java
@@ -3,8 +3,7 @@ package es.maos17.cdr.programacion.ut01.ejemplos;
 public class Ejemplo01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola mundo");
 	}
 
 }
